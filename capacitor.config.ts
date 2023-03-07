599a2c58ed2bdb524dff9ebfbffd1e567da1078a
Lasse Hamborg <lham@ramboll.dk>
2023-03-07 23:07:44 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-tut-chat01',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
